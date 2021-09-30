ae3cdef28f11db83162532ff89ea0b7c28671c4b
CxrlosMX <LuisCRendon131@gmail.com>
2021-09-30 17:21:59 -0500

    Clases agregadas

---
+++ src/aplicationclass/RunAplication.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplicationclass;
+
+/**
+ *
+ * @author: CxrlosMX
+ * @Git-Hub: https://github.com/CxrlosMX
+ * @Phone: 953-212-97-27
+ * @Email: LuisCRendon131@gmail.com
+ * @Date: 30-sep-2021
+ * 
+ */
+public class RunAplication {
+    public static void main(String[] args) {
+        
+    }
+
+}
